3382c3049d50723f8c7d06ae93147894a629e3b7
hassan_99 <michaelvitt@vizzwebsolutions.com>
2023-10-02 12:42:54 +0500

    added the buildsh files

--- test/test_test.go
+++ test/test_test.go
@@ -2,6 +2,7 @@ package test
 
 import (
 	"fmt"
+	"time"
 	"io/ioutil"
 	"net/http"
 	"testing"
@@ -9,20 +10,36 @@ import (
 
 func MakeRequest() (string, error) {
 	url := "http://rules.digiebot.com/apiEndPoint/getAllCoinsHavingTradeSettings/1"
+	//url = "http://rules.digiebot.com/goTest"
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return "", err
 	}
+	token:= "OverLimit#_cgA3s8VSQj"
+		// Set headers to replicate POSTMAN headers
+		req.Header.Add("myToken", token) // Replace with your actual token
+		req.Header.Add("User-Agent", "PostmanRuntime/7.33.0")
+		req.Header.Add("Accept", "*/*")
+		req.Header.Add("Accept-Encoding", "gzip, deflate, br")
+		req.Header.Add("Cache-Control", "no-cache")
+		req.Header.Add("Connection", "keep-alive")
+		req.Header.Add("Host", "localhost:3001") // Replace with your Node.js server's hostname and port
+	//req.Header.Set("Content-Type", "application/json")
+	//req.Header.Set("Authorization", "OverLimit#_ubN7iC5W7D")
+	// Print the request headers for debugging
+	fmt.Println("Request Headers:")
+    fmt.Println(req.Header)
+	// Set a timeout for the request
+	client := &http.Client{Timeout: 10 * time.Second}
 
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", "OverLimit#_ubN7iC5W7D")
-
-	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
 		return "", err
 	}
 	defer resp.Body.Close()
+	fmt.Println("Response Status Code:", resp.Status)
+fmt.Println("Response Headers:")
+fmt.Println(resp.Header)
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
