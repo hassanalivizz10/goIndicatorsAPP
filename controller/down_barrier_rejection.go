c7631b8c5d6ed1e281f74b2b619f166eff0affde
Hassan Ali <hassanalivizz10@gmail.com>
2023-12-09 16:48:06 +0500

    added down barrier rejection values

--- controller/down_barrier_rejection.go
+++ controller/down_barrier_rejection.go
@@ -19,7 +19,7 @@ var coinListCacheForBarrier []bson.M
 var rejectionMutex sync.Mutex
 // Defaults ....
 var wickMoveFactorValue  float64  = 2
-var debug = false
+var debug = true
 
 type BarriersDataStruct struct {
 	Symbol                 		string 
